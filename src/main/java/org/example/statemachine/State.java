c1a665dff1c7199157c20d33803f60f088f1a358
Nikita <nikitabarkovskij44@gmail.com>
2024-04-07 19:09:48 +0300

    StateMachine

---
+++ src/main/java/org/example/statemachine/State.java
@@ -0,0 +1,10 @@
+package org.example.statemachine;
+
+public class State {
+    public final static String WaitingStart = "WaitingStart";
+    public final static String WaitingInstructionOfKnowledge = "WaitingInstructionOfKnowledge";
+    public final static String WaitingShowAllRequests = "WaitingShowAllRequests";
+    public final static String WaitingToAddResponse = "WaitingToAddResponse";
+    public final static String WaitingForUserResponsesToViewed = "WaitingForUserResponsesToViewed";
+
+}
